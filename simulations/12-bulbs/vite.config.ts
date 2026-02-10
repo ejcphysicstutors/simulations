d007ddb79860dc27cd108b4f258e4216eccde4db
ejcphysicstutors <ejc.physics.tutors@ejc.edu.sg>
2026-02-10 14:01:52 +0800

    Add file from ZIP

--- simulations/12-bulbs/vite.config.ts
+++ simulations/12-bulbs/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simulations/simulations/12-bulbs/', // Add this line here!
   server: {
     port: 3000
   }
