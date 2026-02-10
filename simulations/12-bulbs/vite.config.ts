70a08c08078f8fb2c976b6def4a78954bf455478
ejcphysicstutors <ejc.physics.tutors@ejc.edu.sg>
2026-02-10 13:59:37 +0800

    Update base path in Vite configuration

--- simulations/12-bulbs/vite.config.ts
+++ simulations/12-bulbs/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simulations/', // Add this line here!
+  base: '/simulations/simulations/12-bulbs/', // Add this line here!
   server: {
     port: 3000
   }
